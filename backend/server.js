ca13f4adf54a45222c8cb885470b1561d153d38f
Muhammad Rashid Khan <pmik754@gmail.com>
2026-08-06 11:47:17 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+
 const app = require("./src/app");
-const connectDB  = require("./src/config/db");
+const connectDB = require("./src/config/db");
+
 connectDB();
 
-app.listen(5000, () => {
-  console.log("Server running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
 });
